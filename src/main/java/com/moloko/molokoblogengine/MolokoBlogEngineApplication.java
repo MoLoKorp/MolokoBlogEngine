8501f97758a51cd5af7467b046101c87f2717abe
Dmitrij Mironov <mironouz@gmail.com>
2022-06-26 20:24:41 +0200

    init setup

---
+++ src/main/java/com/moloko/molokoblogengine/MolokoBlogEngineApplication.java
@@ -0,0 +1,13 @@
+package com.moloko.molokoblogengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolokoBlogEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolokoBlogEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moloko/molokoblogengine/MolokoBlogEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moloko.molokoblogengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolokoBlogEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
