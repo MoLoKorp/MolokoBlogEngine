d7745808b0217de3649f09c3bf30c362b9df4c67
Dmitrij Mironov <mironouz@gmail.com>
2022-07-28 22:27:35 +0200

    add simple static frontend, enable cors temporaly

--- src/main/java/com/moloko/molokoblogengine/resource/ArticleController.java
+++ src/main/java/com/moloko/molokoblogengine/resource/ArticleController.java
@@ -4,18 +4,22 @@ import com.moloko.molokoblogengine.model.Article;
 import com.moloko.molokoblogengine.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-@RestController("/article")
+@CrossOrigin
+@RestController
+@RequestMapping("/article")
 public class ArticleController {
 
     @Autowired
