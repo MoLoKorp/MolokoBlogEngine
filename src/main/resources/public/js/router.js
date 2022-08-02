c1f4ee24385c09104da5cae161bc00db4fa0af0c
Landlord53 <nikolay.chalykh@yandex.ru>
2022-08-02 09:54:39 +0300

    Add simple SPA logic

---
+++ src/main/resources/public/js/router.js
@@ -0,0 +1,58 @@
+'use strict';
+
+function Router(routes) {
+    try {
+        if(!routes) {
+            throw 'error: routes param is mandatory';
+        }
+
+        this.constructor(routes);
+        this.init();
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+Router.prototype = {
+    routes: undefined,
+    rootElem: undefined,
+
+    constructor: function(routes) {
+        this.routes = routes;
+        this.rootElem = document.getElementById('#molokoApp');
+    },
+
+    init: function() {
+        let r = this.routes;
+        (function(scope, r) {
+            window.addEventListener('hashchange', function(e) {
+                scope.hasChanged(scope, r);
+            });
+        }) (this, r);
+    },
+
+    hasChanged: function(scope, r) {
+        if (window.location.hash.length > 0) {
+            for(let i = 0; i < r.length; i++) {
+                if (r[i].isActiveRoute(window.location.hash.substr(1))) {
+                    scope.goToRoute(r[i].htmlName);
+                }
+            }
+        } else {
+            for(let i = 0; i < r.length; i++) {
+                if(r[i].default) {
+                    scope.goToRoute(r[i].htmlName);
+                }
+            }
+        }
+    },
+
+    goToRoute: function(htmlName) {
+        console.log(htmlName);
+        (function(scope) {
+            fetch('views/' + htmlName)
+            .then((response) => { return response.text(); })
+            .then((html) => { scope.rootElem.innerHTML = html;})
+        }) (this);
+    }
+}
