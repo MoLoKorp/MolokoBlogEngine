a951185e4cc3523e7d9a29af8921cff765a4f459
Dmitrij Mironov <mironouz@gmail.com>
2022-08-29 14:51:40 +0200

    MOLOKOBLOGENGINE-T-29: do not log LB, log real IP instead of LB, log request method

--- backend/src/main/java/com/moloko/molokoblogengine/config/HttpRequestLoggingFilter.java
+++ backend/src/main/java/com/moloko/molokoblogengine/config/HttpRequestLoggingFilter.java
@@ -1,5 +1,6 @@
 package com.moloko.molokoblogengine.config;
 
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -13,18 +14,34 @@ import reactor.core.publisher.Mono;
 /** Simple HttpFilter which logs path, ip address and user-agent of the incoming request. */
 @Component
 public class HttpRequestLoggingFilter implements WebFilter {
-  private Logger log = LoggerFactory.getLogger(HttpRequestLoggingFilter.class);
+  private final Logger log = LoggerFactory.getLogger(HttpRequestLoggingFilter.class);
+  private static final String ELB_USER_AGENT = "ELB-HealthChecker/2.0";
+  private static final String X_FORWARDED_FOR = "X-Forwarded-For";
+  private static final List<String> EXCLUDES = List.of(".js", ".css", ".ico");
 
   @Override
   public Mono<Void> filter(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
 
     ServerHttpRequest request = serverWebExchange.getRequest();
     HttpHeaders headers = request.getHeaders();
-    String from = request.getRemoteAddress().toString();
-    if (headers.containsKey(HttpHeaders.USER_AGENT)) {
-      from += " " + headers.get(HttpHeaders.USER_AGENT);
+    String path = request.getPath().toString();
+    String realIp = "IP is not detected";
+    List<String> userAgents = List.of();
+
+    if (headers.containsKey(X_FORWARDED_FOR) && !headers.get(X_FORWARDED_FOR).isEmpty()) {
+      realIp = headers.get(X_FORWARDED_FOR).get(0);
     }
-    log.info("Incoming request to {} from {}", request.getPath(), from);
+    if (headers.containsKey(HttpHeaders.USER_AGENT)) {
+      userAgents = headers.get(HttpHeaders.USER_AGENT);
+    }
+    if (!userAgents.contains(ELB_USER_AGENT) && !EXCLUDES.stream().anyMatch(path::contains)) {
+      log.info(
+          "Incoming {} request to {} from {} user-agent: {}",
+          request.getMethodValue(),
+          path,
+          realIp,
+          userAgents);
+    }
     return webFilterChain.filter(serverWebExchange);
   }
 }
--- backend/src/test/java/com/moloko/molokoblogengine/config/HttpRequestLoggingFilterTest.java
+++ backend/src/test/java/com/moloko/molokoblogengine/config/HttpRequestLoggingFilterTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
@@ -24,33 +23,39 @@ import org.springframework.web.server.WebFilterChain;
 @ExtendWith(OutputCaptureExtension.class)
 class HttpRequestLoggingFilterTest {
 
+  private static final String X_FORWARDED_FOR = "X-Forwarded-For";
+
   @Mock ServerWebExchange serverWebExchange;
 
   @Mock WebFilterChain webFilterChain;
 
   @Mock ServerHttpRequest serverHttpRequest;
 
-  @Mock InetSocketAddress inetSocketAddress;
-
   @Mock RequestPath requestPath;
 
   @Test
   void testFilter(CapturedOutput log) {
     when(serverWebExchange.getRequest()).thenReturn(serverHttpRequest);
+    when(serverHttpRequest.getMethodValue()).thenReturn("GET");
     when(serverHttpRequest.getPath()).thenReturn(requestPath);
     when(requestPath.toString()).thenReturn("/testPath");
-    when(serverHttpRequest.getRemoteAddress()).thenReturn(inetSocketAddress);
-    when(inetSocketAddress.toString()).thenReturn("1.1.1.1");
     when(serverHttpRequest.getHeaders())
         .thenReturn(
             new HttpHeaders(
                 new LinkedMultiValueMap<>(
-                    Map.of(HttpHeaders.USER_AGENT, List.of("testUserAgent")))));
+                    Map.of(
+                        X_FORWARDED_FOR,
+                        List.of("1.1.1.1"),
+                        HttpHeaders.USER_AGENT,
+                        List.of("testUserAgent")))));
     HttpRequestLoggingFilter httpRequestLoggingFilter = new HttpRequestLoggingFilter();
 
     httpRequestLoggingFilter.filter(serverWebExchange, webFilterChain);
 
-    assertTrue(log.getOut().contains("Incoming request to /testPath from 1.1.1.1 [testUserAgent]"));
+    assertTrue(
+        log.getOut()
+            .contains(
+                "Incoming GET request to /testPath from 1.1.1.1 user-agent: [testUserAgent]"));
     verify(webFilterChain).filter(serverWebExchange);
   }
 }
