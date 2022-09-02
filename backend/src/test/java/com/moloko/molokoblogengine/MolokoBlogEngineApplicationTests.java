76a3c99aa1376aeb5bfdccfe846be4e0b042fe1b
Dmitrij Mironov <mironouz@gmail.com>
2022-09-02 03:52:51 +0200

    MOLOKOBLOGENGINE-T-30: fix test which depends on env var

--- backend/src/test/java/com/moloko/molokoblogengine/MolokoBlogEngineApplicationTests.java
+++ backend/src/test/java/com/moloko/molokoblogengine/MolokoBlogEngineApplicationTests.java
@@ -6,6 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MolokoBlogEngineApplicationTests {
 
+  static {
+    System.setProperty("MONGO_PASSWORD", "mongostub");
+  }
+
   @Test
   void contextLoads() {}
 }
