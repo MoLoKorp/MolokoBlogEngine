00eb2e32b7bbb32400da5f94880e5502ae499db7
Dmitrij Mironov <mironouz@gmail.com>
2022-12-06 02:39:26 +0100

    MOLOKOBLOGENGINE-T-45: use node+npm instead of gradle npm plugin, split CI check and deploy to frontend and backend files

--- frontend/wiremock/MolokoBlogEngineWireMock.java
+++ frontend/wiremock/MolokoBlogEngineWireMock.java
@@ -1,18 +1,13 @@
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-
 import com.github.tomakehurst.wiremock.WireMockServer;
+
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
 class MolokoBlogEngineWireMock {
   private static final String FILES = "frontend/wiremock/mocks/__files";
 
   public static void main(String[] args) throws IOException {
-    copyFilesToWiremockFolder("/views");
-    copyFilesToWiremockFolder("/css");
-
     WireMockServer wireMockServer =
         new WireMockServer(
             options()
@@ -25,23 +20,5 @@ class MolokoBlogEngineWireMock {
     } finally {
       wireMockServer.stop();
     }
-  }
-
-  private static void copyFilesToWiremockFolder(String from) throws IOException {
-    if (Files.notExists(Path.of(FILES + from))) {
-      Files.createDirectory(Path.of(FILES + from));
-    }
-    Files.newDirectoryStream(Path.of("frontend" + from))
-        .forEach(
-            file -> {
-              try {
-                Files.copy(
-                    file,
-                    Path.of(FILES + from + "/" + file.getFileName()),
-                    StandardCopyOption.REPLACE_EXISTING);
-              } catch (IOException e) {
-                throw new RuntimeException(e);
-              }
-            });
   }
 }
